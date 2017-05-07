3af7c64a038c004928102a97a1b766b2a658af10
allen50425752 <50425752@qq.com>
2017-05-07 14:39:29 +0800

    Delete db.inc.php

--- db.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$db_host = '127.0.0.1';
-	$db_user = 'root';
-	$db_password = '';
-	$db_name = 'category';
-	$link = mysqli_connect($db_host,$db_user,$db_password) or die(mysqli_error());
-	mysqli_select_db($link,$db_name) or die(mysqli_error());
-	mysqli_query($link,"set names utf8");
-?>
